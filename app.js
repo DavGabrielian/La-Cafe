610c2a4b46971d9b14d5dcc39ff9d0920f406afa
David Gabrielian <davidgabrielofficial@gmail.com>
2020-08-13 16:09:56 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+let eat = document.getElementById("eat");
+let drink = document.getElementById("drink");
+
+
+  openEat = () => {
+    eat.style.display = "block";
+    drink.style.display = "none";
+  };
+
+
+  openDrink = () => {
+    drink.style.display = "block";
+    eat.style.display = "none";
+  };
+
+
